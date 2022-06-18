ec2b1747f5a1da9063065eb3afe412d94bc62e9e
itsparasbisht <iamparasbisht@gmail.com>
2022-06-18 20:53:52 +0530

    add nodes

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -8,69 +8,43 @@ import ReactFlow, {
 } from "react-flow-renderer";
 import styles from "./graph.module.css";
 
-const initialNodes = [
-  {
-    id: "1",
-    data: { label: "Hi" },
-    style: {
-      color: "black",
-      fontSize: "15px",
-      fontWeight: "bold",
-      backgroundColor: "cyan",
-      fontFamily: "monospace",
-    },
-    position: { x: 50, y: 50 },
-  },
-  {
-    id: "2",
-    data: {
-      label: (
-        <ol>
-          <li>go on a ride</li>
-          <li>on a date</li>
-          <li>riding</li>
-        </ol>
-      ),
-    },
-    style: {
-      color: "red",
-      fontSize: "15px",
-      fontWeight: "bold",
-      backgroundColor: "yellow",
-      fontFamily: "monospace",
-    },
-    position: { x: 200, y: 140 },
-  },
-  {
-    id: "3",
-    data: { label: "hello" },
-    style: {
-      color: "black",
-      fontSize: "15px",
-      fontWeight: "bold",
-      backgroundColor: "cyan",
-      fontFamily: "monospace",
-    },
-    position: { x: 550, y: 50 },
-  },
-];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialNodes = [];
+const initialEdges = [];
 
 const graphStyles = { width: "100%", height: "500px" };
 
 function Graph() {
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
+  const [newNode, setNewNode] = useState("");
 
   const onNodesChange = useCallback(
     (changes) => setNodes((ns) => applyNodeChanges(changes, ns)),
     []
   );
+
   const onEdgesChange = useCallback(
     (changes) => setEdges((es) => applyEdgeChanges(changes, es)),
     []
   );
+
   const onConnect = (connection) => setEdges((eds) => addEdge(connection, eds));
+
+  const addNode = () => {
+    const obj = {
+      id: "4",
+      data: { label: newNode },
+      style: {
+        color: "black",
+        fontSize: "15px",
+        fontWeight: "bold",
+        backgroundColor: "cyan",
+        fontFamily: "monospace",
+      },
+      position: { x: 550, y: 250 },
+    };
+    setNodes([...nodes, obj]);
+  };
 
   return (
     <>
@@ -85,7 +59,14 @@ function Graph() {
         <MiniMap />
         <Controls />
       </ReactFlow>
-      <div className={styles.addNodes}></div>
+      <div className={styles.addNodes}>
+        <input
+          value={newNode}
+          onChange={(e) => setNewNode(e.target.value)}
+          type="text"
+        />
+        <button onClick={addNode}>add</button>
+      </div>
     </>
   );
 }
