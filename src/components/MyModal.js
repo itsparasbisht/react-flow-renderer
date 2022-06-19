7045b5ef56bb2e40415f40fdb325eaa689854e09
itsparasbisht <iamparasbisht@gmail.com>
2022-06-19 13:11:04 +0530

    my modal

--- src/components/MyModal.js
+++ src/components/MyModal.js
@@ -1,15 +1,16 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import { Button, Stack, TextField } from "@mui/material";
+import styles from "./myModal.module.css";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: "60%",
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
@@ -26,12 +27,48 @@ export default function MyModal({ handler }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Enter label for the node"
+            variant="outlined"
+          />
+          <Typography variant="subtitle2" textAlign={"center"} mt={2}>
+            You can add sub nodes to your current node
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <div className={styles.subNodesContainer}>
+            <TextField
+              size="small"
+              id="outlined-basic"
+              label="sub node 1"
+              variant="outlined"
+            />
+            <TextField
+              size="small"
+              id="outlined-basic"
+              label="sub node 2"
+              variant="outlined"
+            />
+            <TextField
+              size="small"
+              id="outlined-basic"
+              label="sub node 3"
+              variant="outlined"
+            />
+          </div>
+          <Stack alignItems={"center"}>
+            <Button
+              sx={{
+                width: "200px",
+                marginTop: "10px",
+                backgroundColor: "black",
+              }}
+              disableElevation
+              variant="contained"
+            >
+              Submit
+            </Button>
+          </Stack>
         </Box>
       </Modal>
     </div>
