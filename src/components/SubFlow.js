fcb3beeb15dc5c77401408daf4e448f63b49589f
itsparasbisht <iamparasbisht@gmail.com>
2022-06-19 12:12:29 +0530

    sub flow

--- src/components/SubFlow.js
+++ src/components/SubFlow.js
@@ -8,12 +8,63 @@ import ReactFlow, {
 } from "react-flow-renderer";
 import styles from "./graph.module.css";
 
-const initialNodes = [];
+const initialNodes = [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "hello" },
+    position: { x: 250, y: 5 },
+    className: "light",
+  },
+  {
+    id: "2",
+    data: { label: "Choose your options" },
+    position: { x: 100, y: 100 },
+    className: "light",
+    style: { backgroundColor: "rgba(255, 0, 0, 0.2)", width: 600, height: 100 },
+  },
+  {
+    id: "2a",
+    data: { label: "1. login" },
+    position: { x: 10, y: 50 },
+    parentNode: "2",
+  },
+  {
+    id: "2b",
+    data: { label: "2. Signup" },
+    position: { x: 200, y: 50 },
+    parentNode: "2",
+  },
+  {
+    id: "2c",
+    data: { label: "3. Assistance" },
+    position: { x: 400, y: 50 },
+    parentNode: "2",
+  },
+  {
+    id: "3",
+    data: { label: "Enter your password" },
+    position: { x: 400, y: 200 },
+    parentNode: "2",
+  },
+  {
+    id: "4",
+    data: { label: "Enter your email" },
+    position: { x: 400, y: 300 },
+    parentNode: "2",
+  },
+  {
+    id: "5",
+    data: { label: "Enter your Phone no." },
+    position: { x: 400, y: 400 },
+    parentNode: "2",
+  },
+];
 const initialEdges = [];
 
 const graphStyles = { width: "100%", height: "500px" };
 
-function Graph() {
+function SubFlow() {
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
   const [newNode, setNewNode] = useState("");
@@ -81,4 +132,4 @@ function Graph() {
   );
 }
 
-export default Graph;
+export default SubFlow;
